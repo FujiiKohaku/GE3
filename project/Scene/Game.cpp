c8b5af7cff7773eb287a536cf954ac904c674633
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-11-17 16:58:41 +0900

    2値抜き終わった

--- project/Scene/Game.cpp
+++ project/Scene/Game.cpp
@@ -31,9 +31,9 @@ void Game::Initialize(WinApp* winApp, DirectXCommon* dxCommon)
     ModelManager::GetInstance()->LoadModel("plane.obj");
     ModelManager::GetInstance()->LoadModel("axis.obj");
     ModelManager::GetInstance()->LoadModel("titleTex.obj");
-
+    ModelManager::GetInstance()->LoadModel("fence.obj");
     player2_.Initialize(object3dManager_);
-    player2_.SetModel("axis.obj");
+    player2_.SetModel("fence.obj");
     player2_.SetTranslate({ 3.0f, 0.0f, 0.0f });
     player2_.SetRotate({ std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float>, 0.0f });
 #pragma endregion
