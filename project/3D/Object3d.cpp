37871f176221b6526bfb85e821b3573fe896d312
Master Fujii <k024g0078@g.neec.ac.jp>
2026-01-13 11:50:17 +0900

    assinp

--- project/3D/Object3d.cpp
+++ project/3D/Object3d.cpp
@@ -44,7 +44,7 @@ void Object3d::Initialize(Object3dManager* object3DManager)
     materialData_->enableLighting = true;
     materialData_->uvTransform = MatrixMath::MakeIdentity4x4();
     materialData_->shininess = 32.0f;
-    
+
     // ================================
     // Transform初期値設定
     // ================================
@@ -115,80 +115,60 @@ ModelData Object3d::LoadObjFile(const std::string& directoryPath, const std::str
 {
     // 1.中で必要となる変数の宣言
     ModelData modelData; // 構築するModelData
-    std::vector<Vector4> positions; // 位置
-    std::vector<Vector3> normals; // 法線
-    std::vector<Vector2> texcoords; // テクスチャ座標
-    std::string line; // ファイルから読んだ一行を格納するもの
+    //ファイルから読んだ一行を格納するもの
 
-    // 2.ファイルを開く
-    std::ifstream file(directoryPath + "/" + filename); // ファイルを開く
-    assert(file.is_open()); // とりあえず開けなかったら止める
+    Assimp::Importer importer;
+    std::string filePath = directoryPath + "/" + filename;
 
-    // 3.実際にファイルを読み,ModelDataを構築していく
-    while (std::getline(file, line)) {
-        std::string identifiler;
-        std::istringstream s(line);
-        s >> identifiler; // 先頭の識別子を読む
+  const aiScene* scene = importer.ReadFile(
+        filePath.c_str(),
+        aiProcess_Triangulate | aiProcess_FlipWindingOrder | aiProcess_FlipUVs);
 
-        // identifierに応じた処理
-        if (identifiler == "v") {
-            Vector4 position;
-            s >> position.x >> position.y >> position.z;
-            // 左手座標にする
-            position.x *= -1.0f;
 
-            position.w = 1.0f;
-            positions.push_back(position);
-        } else if (identifiler == "vt") {
-            Vector2 texcoord;
-            s >> texcoord.x >> texcoord.y;
-            // 上下逆にする
+    assert(scene->HasMeshes());
+    // 3.実際にファイルを読み、ModelDataを構築していく
+    for (uint32_t meshIndex = 0; meshIndex < scene->mNumMeshes; ++meshIndex) {
+        aiMesh* mesh = scene->mMeshes[meshIndex];
 
-            // texcoord.y *= -1.0f;
-            texcoord.y = 1.0f - texcoord.y;
-            // CG2_06_02_kusokusosjsusuawihoafwhgiuwhkgfau
-            texcoords.push_back(texcoord);
-        } else if (identifiler == "vn") {
-            Vector3 normal;
-            s >> normal.x >> normal.y >> normal.z;
-            // 左手座標にする
-            normal.x *= -1.0f;
+        assert(mesh->HasNormals());
+        assert(mesh->HasTextureCoords(0));
 
-            normals.push_back(normal);
-        } else if (identifiler == "f") {
-            VertexData triangle[3]; // 三つの頂点を保存
-            // 面は三角形限定。その他は未対応
-            for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
-                std::string vertexDefinition;
-                s >> vertexDefinition;
-                // 頂点の要素へのIndexは「位置/UV/法線」で格納されているので、分解してえIndexを取得する
-                std::istringstream v(vertexDefinition);
-                uint32_t elementIndices[3];
-                for (int32_t element = 0; element < 3; ++element) {
-                    std::string index;
+        for (uint32_t faceIndex = 0; faceIndex < mesh->mNumFaces; ++faceIndex) {
+            aiFace& face = mesh->mFaces[faceIndex];
+            assert(face.mNumIndices == 3); // 三角形のみサポート
+            for (uint32_t element = 0; element < face.mNumIndices; ++element) {
 
-                    std::getline(v, index, '/'); // 区切りでインデックスを読んでいく
-                    elementIndices[element] = std::stoi(index);
-                }
-                // 要素へのIndexから、実際の要素の値を取得して、頂点を構築する
-                Vector4 position = positions[elementIndices[0] - 1];
-                Vector2 texcoord = texcoords[elementIndices[1] - 1];
-                Vector3 normal = normals[elementIndices[2] - 1];
-                // X軸を反転して左手座標系に
+                uint32_t vertexIndex = face.mIndices[element];
 
-                triangle[faceVertex] = { position, texcoord, normal };
+                aiVector3D position = mesh->mVertices[vertexIndex];
+                aiVector3D normal = mesh->mNormals[vertexIndex];
+                aiVector3D texcoord = mesh->mTextureCoords[0][vertexIndex];
+
+                VertexData vertex {};
+
+                vertex.position = { position.x, position.y, position.z, 1.0f };
+                vertex.normal = { normal.x, normal.y, normal.z };
+                vertex.texcoord = { texcoord.x, texcoord.y };
+
+                // aiProcess_MakeLeftHanded は z を -1 で反転するが、
+                // 右手→左手変換をするので手動で対応
+                vertex.position.x *= -1.0f;
+                vertex.normal.x *= -1.0f;
+
+                modelData.vertices.push_back(vertex);
             }
-            // 逆順にして格納（2 → 1 → 0）
-            modelData.vertices.push_back(triangle[2]);
-            modelData.vertices.push_back(triangle[1]);
-            modelData.vertices.push_back(triangle[0]);
-            //?
-        } else if (identifiler == "mtllib") {
-            // materialTemplateLibraryファイルの名前を取得する
-            std::string materialFilename;
-            s >> materialFilename;
-            // 基本的にobjファイルと同一階層mtlは存在させるので、ディレクトリ名とファイル名を渡す。
-            modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
+        }
+    }
+    for (uint32_t materialIndex = 0; materialIndex < scene->mNumMaterials; ++materialIndex) {
+
+        aiMaterial* material = scene->mMaterials[materialIndex];
+
+        if (material->GetTextureCount(aiTextureType_DIFFUSE) != 0) {
+
+            aiString textureFilePath;
+            material->GetTexture(aiTextureType_DIFFUSE, 0, &textureFilePath);
+
+            modelData.material.textureFilePath = directoryPath + "/" + textureFilePath.C_Str();
         }
     }
     // 4.ModelDataを返す
@@ -197,34 +177,34 @@ ModelData Object3d::LoadObjFile(const std::string& directoryPath, const std::str
 #pragma endregion
 
 #pragma region MTL読み込み処理
-// ===============================================
-// マテリアル（.mtl）ファイルの読み込み
-// ===============================================
-MaterialData Object3d::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
-{
-
-    // 1.中で必要となる変数の宣言
-    MaterialData materialData; // 構築するMaterialData
-    // 2.ファイルを開く
-    std::string line; // ファイルから読んだ１行を格納するもの
-    std::ifstream file(directoryPath + "/" + filename); // ファイルを開く
-    assert(file.is_open()); // とりあえず開けなかったら止める
-    // 3.実際にファイルを読み、MaterialDataを構築していく
-    while (std::getline(file, line)) {
-        std::string identifier;
-        std::istringstream s(line);
-        s >> identifier;
-        // identifierに応じた処理
-        if (identifier == "map_Kd") {
-            std::string textureFilename;
-            s >> textureFilename;
-            // 連結してファイルパスにする
-            materialData.textureFilePath = directoryPath + "/" + textureFilename;
-        }
-    }
-    // 4.materialDataを返す
-    return materialData;
-}
+//// ===============================================
+//// マテリアル（.mtl）ファイルの読み込み
+//// ===============================================
+//MaterialData Object3d::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
+//{
+//
+//    // 1.中で必要となる変数の宣言
+//    MaterialData materialData; // 構築するMaterialData
+//    // 2.ファイルを開く
+//    std::string line; // ファイルから読んだ１行を格納するもの
+//    std::ifstream file(directoryPath + "/" + filename); // ファイルを開く
+//    assert(file.is_open()); // とりあえず開けなかったら止める
+//    // 3.実際にファイルを読み、MaterialDataを構築していく
+//    while (std::getline(file, line)) {
+//        std::string identifier;
+//        std::istringstream s(line);
+//        s >> identifier;
+//        // identifierに応じた処理
+//        if (identifier == "map_Kd") {
+//            std::string textureFilename;
+//            s >> textureFilename;
+//            // 連結してファイルパスにする
+//            materialData.textureFilePath = directoryPath + "/" + textureFilename;
+//        }
+//    }
+//    // 4.materialDataを返す
+//    return materialData;
+//}
 
 #pragma endregion
 
