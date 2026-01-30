f61bda9c9ac549e1dca7f1cb659f812ad52e25f7
Master Fujii <k024g0078@g.neec.ac.jp>
2026-01-30 17:45:27 +0900

    a

--- project/resources/shaders/Object3d.VS.hlsl
+++ project/resources/shaders/Object3d.VS.hlsl
@@ -22,7 +22,10 @@ VertexShaderOutput main(VertexSgaderInput input)
     VertexShaderOutput output;
     output.position = mul(input.position, gTransformationMatrix.WVP);
     output.texcoord = input.texcoord;
-    output.normal = normalize( mul((float32_t3x3) gTransformationMatrix.WorldInverseTranspose, input.normal));
+    output.normal = normalize(mul(input.normal,(float32_t3x3) gTransformationMatrix.WorldInverseTranspose)
+);
+
+
     output.worldPosition =mul(input.position, gTransformationMatrix.World).xyz;
 
     return output;
