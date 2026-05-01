ad08b7ce796da9493e7f9550d47536ad07582915
Master Fujii <k024g0078@g.neec.ac.jp>
2026-05-01 12:36:33 +0900

    かっくいいー

--- project/resources/shaders/UpdateParticle.CS.hlsl
+++ project/resources/shaders/UpdateParticle.CS.hlsl
@@ -25,8 +25,30 @@ void main(uint32_t3 DTid : SV_DispatchThreadID)
 
     gParticles[index].currentTime += gPerFrame.deltaTime;
 
-    float alpha =
-        1.0f - (gParticles[index].currentTime / gParticles[index].lifeTime);
+    float lifeRate =
+        gParticles[index].currentTime / gParticles[index].lifeTime;
 
+    lifeRate = saturate(lifeRate);
+
+    float3 startColor = float3(1.0f, 1.0f, 0.8f);
+    float3 middleColor = float3(1.0f, 0.45f, 0.0f);
+    float3 endColor = float3(0.15f, 0.03f, 0.0f);
+
+    float3 currentColor;
+
+    if (lifeRate < 0.25f)
+    {
+        float t = lifeRate / 0.25f;
+        currentColor = lerp(startColor, middleColor, t);
+    }
+    else
+    {
+        float t = (lifeRate - 0.25f) / 0.75f;
+        currentColor = lerp(middleColor, endColor, t);
+    }
+
+    gParticles[index].color.rgb = currentColor;
+
+    float alpha = 1.0f - lifeRate;
     gParticles[index].color.a = saturate(alpha);
 }
