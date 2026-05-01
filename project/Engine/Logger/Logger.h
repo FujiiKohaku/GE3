11bab0e599b1e3be0223d8d2af48864e2f13617d
Master Fujii <k024g0078@g.neec.ac.jp>
2026-05-01 13:46:43 +0900

    LOGをいじる

--- project/Engine/Logger/Logger.h
+++ project/Engine/Logger/Logger.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <fstream>
 #include <string>
 
 class Logger {
 public:
+    static void Initialize();
+    static void Finalize();
+
     static void Log(const std::string& message);
+    static void Error(const std::string& message);
+    static std::string GetTimeString();
+
+private:
+    static std::ofstream logFile_;
 };
