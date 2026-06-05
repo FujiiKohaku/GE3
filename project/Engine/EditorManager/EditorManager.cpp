8aecb9ebce0ee62879d3d234b2decb133df4ecd9
Master Fujii <k024g0078@g.neec.ac.jp>
2026-06-05 23:16:41 +0900

    なぜうごく

---
+++ project/Engine/EditorManager/EditorManager.cpp
@@ -0,0 +1,30 @@
+#include "EditorManager.h"
+
+#ifdef USE_IMGUI
+#include "../externals/imgui/imgui.h"
+#endif
+
+void EditorManager::Initialize()
+{
+}
+
+void EditorManager::Update()
+{
+}
+
+void EditorManager::DrawImGui()
+{
+#ifdef USE_IMGUI
+
+    ImGui::Begin("Editor");
+
+    if (selectedObject_ == nullptr) {
+        ImGui::Text("No Selection");
+    } else {
+        ImGui::Text("Object Selected");
+    }
+
+    ImGui::End();
+
+#endif
+}
