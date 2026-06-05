fadd26047aeae7e5f60a17b214f9f5b67c2f006b
Master Fujii <k024g0078@g.neec.ac.jp>
2026-06-05 23:37:45 +0900

    a

--- project/Engine/math/MatrixMath.h
+++ project/Engine/math/MatrixMath.h
@@ -18,5 +18,5 @@ public:
     static Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
     static Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
     static Matrix4x4 Transpose(const Matrix4x4& m);
-    void DecomposeMatrix(const Matrix4x4& matrix,Vector3& scale,Vector3& translate);
+    static void DecomposeMatrix(const Matrix4x4& matrix, Vector3& scale, Vector3& translate);
 };
